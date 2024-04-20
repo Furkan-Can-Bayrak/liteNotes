da7ebc809d4b220eddbd029d68eeea170979cc06
Furkan Can Bayrak <furkancanb957@gmail.com>
2024-04-20 20:07:05 +0300

    h2 içine <a> eklendi

--- resources/views/front/notes/index.blade.php
+++ resources/views/front/notes/index.blade.php
@@ -17,7 +17,7 @@
     @if($notes->count() > 0)
         @foreach($notes as $note)
             <div class="border-bottom shadow-sm p-3 mb-5 bg-body rounded-3 mb-3 p-3">
-                <h2 class="fs-2 fw-bold">{{$note->title}}</h2>
+                <h2 class="fs-2 fw-bold"><a href="{{route('showNote',$note->id)}}" class="text-black text-decoration-none">{{$note->title}}</a></h2>
                 <p class="mt-3">{{Str::limit($note->content,130)  }}</p>
                <span class="block fs-6 text-muted opacity-50"> {{$note->updated_at->diffForHumans()}}</span>
             </div>
